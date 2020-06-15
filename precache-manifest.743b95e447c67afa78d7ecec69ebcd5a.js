2c1854e42bc5187ecd7dc9924681dd1dc7b17745
xchai3 <xipengc@andrew.cmu.edu>
2020-06-14 18:28:53 -0700

    Updates

---
+++ precache-manifest.743b95e447c67afa78d7ecec69ebcd5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82ee15ba07f331d05b4524b28bd0a946",
+    "url": "/xchai3.github.io/index.html"
+  },
+  {
+    "revision": "5924b6d3e2ba5ad2a111",
+    "url": "/xchai3.github.io/static/css/main.bf5e5dff.chunk.css"
+  },
+  {
+    "revision": "366fc81e113e53894932",
+    "url": "/xchai3.github.io/static/js/2.646d36e2.chunk.js"
+  },
+  {
+    "revision": "971b4e562d3abe5fe0995509ffca7edb",
+    "url": "/xchai3.github.io/static/js/2.646d36e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5924b6d3e2ba5ad2a111",
+    "url": "/xchai3.github.io/static/js/main.aa3dd12a.chunk.js"
+  },
+  {
+    "revision": "b5c1d8519bc1567340e9",
+    "url": "/xchai3.github.io/static/js/runtime-main.11f54c63.js"
+  },
+  {
+    "revision": "4cffa739f751f90686ec3ac6e9b56e0d",
+    "url": "/xchai3.github.io/static/media/icon-check.4cffa739.svg"
+  }
+]);
