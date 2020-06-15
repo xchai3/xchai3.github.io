95cfc9687f5e316324799857f14da6b53e4dd1b2
xchai3 <xipengc@andrew.cmu.edu>
2020-06-14 20:18:51 -0700

    again

---
+++ precache-manifest.8b3e9a5fb2694ef6a206aca2113ecc42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f55d7586ad11c2cf3f145fa9ce3bc3d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7e0dabd931e8d1b836a",
+    "url": "/static/css/main.9aa7ba73.chunk.css"
+  },
+  {
+    "revision": "366fc81e113e53894932",
+    "url": "/static/js/2.646d36e2.chunk.js"
+  },
+  {
+    "revision": "971b4e562d3abe5fe0995509ffca7edb",
+    "url": "/static/js/2.646d36e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7e0dabd931e8d1b836a",
+    "url": "/static/js/main.8c8e9c99.chunk.js"
+  },
+  {
+    "revision": "2a1493d5ef492a2503ec",
+    "url": "/static/js/runtime-main.92710613.js"
+  },
+  {
+    "revision": "4cffa739f751f90686ec3ac6e9b56e0d",
+    "url": "/static/media/icon-check.4cffa739.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.46efec5b13810166ccc6bbcecef482c1.js"
+  "/precache-manifest.8b3e9a5fb2694ef6a206aca2113ecc42.js"
 );
 
 self.addEventListener('message', (event) => {
